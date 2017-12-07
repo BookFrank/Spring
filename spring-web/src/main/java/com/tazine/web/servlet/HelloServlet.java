c181044a9194145faad2b5032e9cb82f8ff0c72e
Frank <bookfrank@foxmail.com>
2017-12-07 23:16:34 +0800

    Servlet 开始

---
+++ spring-web/src/main/java/com/tazine/web/servlet/HelloServlet.java
@@ -0,0 +1,20 @@
+package com.tazine.web.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 测试 Servlet
+ *
+ * @author Frank
+ */
+public class HelloServlet extends HttpServlet{
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+}
