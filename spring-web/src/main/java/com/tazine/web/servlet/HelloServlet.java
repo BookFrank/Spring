181476296e58331a0119015c8813744ece6e1345
Frank <bookfrank@foxmail.com>
2017-12-10 23:30:58 +0800

    style (spring-web) : Format the comment for the HelloServlet.
    Format the comment.
    
    Spring cool.

--- spring-web/src/main/java/com/tazine/web/servlet/HelloServlet.java
+++ spring-web/src/main/java/com/tazine/web/servlet/HelloServlet.java
@@ -1,20 +1,30 @@
 package com.tazine.web.servlet;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * 测试 Servlet
  *
  * @author Frank
+ * @since 1.0.0
  */
-public class HelloServlet extends HttpServlet{
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String currentTime = df.format(new Date());
+        req.setAttribute("currentTime", currentTime);
+        req.setAttribute("ll", "liyang");
+        req.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(req, resp);
     }
 }
