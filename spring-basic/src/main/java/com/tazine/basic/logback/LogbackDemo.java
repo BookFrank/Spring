ce63a11f0c40754df79dffdf9e33d2cc997d4b0f
Frank <bookfrank@foxmail.com>
2017-12-12 22:10:37 +0800

    style (spring-basic) : Reformat the comment and code.
    Reformat the code and xml.
    
    Practice makes prefect.

--- spring-basic/src/main/java/com/tazine/basic/logback/LogbackDemo.java
+++ spring-basic/src/main/java/com/tazine/basic/logback/LogbackDemo.java
@@ -1,10 +1,15 @@
 package com.tazine.basic.logback;
 
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.core.util.StatusPrinter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
+ * Logback 异步输出线程实践
  *
+ * @author Frank
+ * @since 1,0,0
  */
 public class LogbackDemo {
 
@@ -12,10 +17,15 @@ public class LogbackDemo {
 
     public static void main(String[] args) {
 
-        while (true){
+        // JPS 查看当前程序进程号
+        // jstack -l number 查看线程运行状态
+        while (true) {
             logger.info("Logback 异步打印 info 日志");
             logger.debug("Logback 异步打印 debug 日志");
             logger.warn("Logback 异步打印 warn 日志");
+
+            // LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+            // StatusPrinter.print(lc);
 
             try {
                 Thread.sleep(5000);
@@ -23,7 +33,5 @@ public class LogbackDemo {
                 e.printStackTrace();
             }
         }
-
     }
-
 }
