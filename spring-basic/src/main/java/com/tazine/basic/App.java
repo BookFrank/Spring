baeb2fcb071a067261edd8a6df5b0b33ef3759c7
Frank <bookfrank@foxmail.com>
2017-12-12 21:52:28 +0800

    feat (spring-basic) : Add the spring-basic module, practice the common util.
    Practice makes prefect.
    
    Spring

---
+++ spring-basic/src/main/java/com/tazine/basic/App.java
@@ -0,0 +1,13 @@
+package com.tazine.basic;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
