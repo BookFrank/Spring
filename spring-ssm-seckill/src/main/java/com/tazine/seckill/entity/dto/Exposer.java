7332d12ec1e4502e937904db62faffe458cfb5ab
Frank <bookfrank@foxmail.com>
2018-01-01 23:38:22 +0800

    feat (spring-ssm-seckill) : add the SeckillService dto and package.

---
+++ spring-ssm-seckill/src/main/java/com/tazine/seckill/entity/dto/Exposer.java
@@ -0,0 +1,88 @@
+package com.tazine.seckill.entity.dto;
+
+/**
+ * 暴露秒杀地址 DTO
+ *
+ * @author frank
+ * @since 1.0.0
+ */
+public class Exposer {
+
+    private boolean exposed;
+
+    private String md5;
+
+    private long seckillId;
+
+    private long now;
+
+    private long start;
+
+    private long end;
+
+    public Exposer(boolean exposed, String md5, long seckillId) {
+        this.exposed = exposed;
+        this.md5 = md5;
+        this.seckillId = seckillId;
+    }
+
+    public Exposer(boolean exposed, long now, long start, long end) {
+        this.exposed = exposed;
+        this.now = now;
+        this.start = start;
+        this.end = end;
+    }
+
+    public Exposer(boolean exposed, long seckillId) {
+        this.exposed = exposed;
+        this.seckillId = seckillId;
+    }
+
+    public boolean isExposed() {
+        return exposed;
+    }
+
+    public void setExposed(boolean exposed) {
+        this.exposed = exposed;
+    }
+
+    public String getMd5() {
+        return md5;
+    }
+
+    public void setMd5(String md5) {
+        this.md5 = md5;
+    }
+
+    public long getSeckillId() {
+        return seckillId;
+    }
+
+    public void setSeckillId(long seckillId) {
+        this.seckillId = seckillId;
+    }
+
+    public long getNow() {
+        return now;
+    }
+
+    public void setNow(long now) {
+        this.now = now;
+    }
+
+    public long getStart() {
+        return start;
+    }
+
+    public void setStart(long start) {
+        this.start = start;
+    }
+
+    public long getEnd() {
+        return end;
+    }
+
+    public void setEnd(long end) {
+        this.end = end;
+    }
+}
---
+++ spring-ssm-seckill/src/main/java/com/tazine/seckill/service/SeckillService.java
@@ -0,0 +1,36 @@
+package com.tazine.seckill.service;
+
+import com.tazine.seckill.entity.Seckill;
+import com.tazine.seckill.entity.dto.Exposer;
+
+import java.util.List;
+
+/**
+ * 秒杀 Web 接口
+ *
+ * @author frank
+ * @since 1.0.0
+ */
+public interface SeckillService {
+
+    /**
+     * 查询所有秒杀记录
+     *
+     * @return
+     */
+    List<Seckill> getSeckillList();
+
+    Seckill getById(long seckillId);
+
+    /**
+     * 秒杀开启时，输出秒杀接口地址，否则输出系统时间和秒杀时间
+     *
+     * @param seckillId
+     * @return
+     */
+    Exposer exposeSeckillUrl(long seckillId);
+
+    void executeSeckill(long seckillId, long userPhone, String md5);
+
+
+}
