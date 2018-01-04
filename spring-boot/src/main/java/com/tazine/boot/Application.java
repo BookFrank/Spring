7c9b5588058afeddeed4062af854afe0634e161b
Frank <bookfrank@foxmail.com>
2018-01-04 23:40:42 +0800

    feat (spring-boot) : the timer tasks practice in spring-boot.

--- spring-boot/src/main/java/com/tazine/boot/Application.java
+++ spring-boot/src/main/java/com/tazine/boot/Application.java
@@ -3,9 +3,11 @@ package com.tazine.boot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.tazine.boot")
+@EnableScheduling
 public class Application {
 
 	public static void main(String[] args) {
