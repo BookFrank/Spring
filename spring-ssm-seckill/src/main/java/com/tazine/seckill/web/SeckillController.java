d8968fb4b24fea94ad2eb096a019cb4333c7989b
Frank <bookfrank@foxmail.com>
2018-01-04 23:16:58 +0800

    feat (spring-ssm-seckill) : springmvc seckill controller.

--- spring-ssm-seckill/src/main/java/com/tazine/seckill/web/SeckillController.java
+++ spring-ssm-seckill/src/main/java/com/tazine/seckill/web/SeckillController.java
@@ -1,15 +1,15 @@
 package com.tazine.seckill.web;
 
 import com.tazine.seckill.dto.Exposer;
+import com.tazine.seckill.dto.SeckillExecution;
 import com.tazine.seckill.dto.SeckillResult;
 import com.tazine.seckill.entity.Seckill;
+import com.tazine.seckill.exception.SeckillException;
 import com.tazine.seckill.service.SeckillService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,7 +22,6 @@ import java.util.List;
 @Controller
 @RequestMapping("/seckill")
 public class SeckillController {
-
 
     @Autowired
     private SeckillService seckillService;
@@ -46,10 +45,40 @@ public class SeckillController {
         return "detail";
     }
 
-    @RequestMapping(value = "/{seckillId}/exposer",method = RequestMethod.GET)
+    @RequestMapping(value = "/{seckillId}/exposer",
+            method = RequestMethod.POST,
+            produces = {"application/json;charset=UTF-8"})
+    @ResponseBody
     private SeckillResult<Exposer> exposer(Long seckillId){
-        SeckillResult<Exposer> ret = new SeckillResult<Exposer>();
-        
+        SeckillResult<Exposer> ret;
+        try {
+            Exposer exposer = seckillService.exposeSeckillUrl(seckillId);
+            ret = new SeckillResult<>(true, exposer);
+        } catch (Exception e) {
+            e.printStackTrace();
+            ret = new SeckillResult<>(false, e.getMessage());
+        }
+        return ret;
+    }
+
+    @RequestMapping(value = "/{seckillId}/{md5}/execution",
+            method = RequestMethod.POST,
+            produces = {"application/json;charset=UTF-8"})
+    @ResponseBody
+    public SeckillResult<SeckillExecution> execute(@PathVariable Long seckillId,@PathVariable String md5, @CookieValue(value = "killPhone", required = false) Long userPhone){
+        SeckillResult<SeckillExecution> ret = null;
+        // springmvc valid
+        if (null == userPhone){
+            return new SeckillResult<>(false,"未注册");
+        }
+        try {
+            SeckillExecution execution = seckillService.executeSeckill(seckillId,userPhone,md5);
+            ret = new SeckillResult<>(true, execution);
+        } catch (SeckillException e) {
+            e.printStackTrace();
+            ret = new SeckillResult<>(false,e.getMessage());
+        }
+        return ret;
     }
 
 
