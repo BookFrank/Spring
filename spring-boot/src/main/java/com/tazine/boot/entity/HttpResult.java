ed852e6c6692647d8776a62af29f185e9ef70965
Frank <bookfrank@foxmail.com>
2018-01-09 18:39:37 +0800

    feat (spring-boot) : write the method in JPARepository to realize the findByX.

---
+++ spring-boot/src/main/java/com/tazine/boot/entity/HttpResult.java
@@ -0,0 +1,40 @@
+package com.tazine.boot.entity;
+
+/**
+ * HTTP请求最外层响应体
+ *
+ * @author frank
+ * @since 1.0.0
+ */
+public class HttpResult<T> {
+
+    private int code;
+
+    private String msg;
+
+    private T data;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
--- spring-boot/src/main/java/com/tazine/boot/repository/PlayerRepository.java
+++ spring-boot/src/main/java/com/tazine/boot/repository/PlayerRepository.java
@@ -6,13 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 /**
- * Created by lina on 2018/1/8.
+ * PlayerRepository
  *
  * @author frank
  * @since 1.0.0
  */
 public interface PlayerRepository extends JpaRepository<Player,Integer>{
-
 
     public List<Player> findByName(String name);
 
