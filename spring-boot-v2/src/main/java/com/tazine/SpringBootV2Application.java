d2276b192a5a379bc9128595b9787e6aa5d7163d
Frank <bookfrank@foxmail.com>
2018-01-24 23:34:44 +0800

    feat (spring-boot-v2) : add the spring-boot-v2 module to practice the webflux.

---
+++ spring-boot-v2/src/main/java/com/tazine/SpringBootV2Application.java
@@ -0,0 +1,12 @@
+package com.tazine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootV2Application.class, args);
+	}
+}
---
+++ spring-boot/src/test/java/com/tazine/boot/aop/AmqpAppTest.java
@@ -0,0 +1,19 @@
+package com.tazine.boot.aop;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+/**
+ * Aop Package Tests
+ *
+ * @author frank
+ * @since 1.0.0
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AmqpAppTest {
+
+
+}
