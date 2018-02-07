31c5d030533acff77f070fb4b8e6ecd1e8fd68cc
Frank <bookfrank@foxmail.com>
2018-02-07 23:40:03 +0800

    feat (spring-boot-mybatis) : add the entity.

---
+++ spring-boot-mybatis/src/main/java/com/tazine/boot/entity/Area.java
@@ -0,0 +1,62 @@
+package com.tazine.boot.entity;
+
+import java.util.Date;
+
+/**
+ * Area
+ *
+ * @author frank
+ * @since 1.0.0
+ */
+public class Area {
+
+    private Integer areaId;
+
+    private String areaName;
+
+    private Integer priority;
+
+    private Date createTime;
+
+    private Date lastModifyTime;
+
+    public Integer getAreaId() {
+        return areaId;
+    }
+
+    public void setAreaId(Integer areaId) {
+        this.areaId = areaId;
+    }
+
+    public String getAreaName() {
+        return areaName;
+    }
+
+    public void setAreaName(String areaName) {
+        this.areaName = areaName;
+    }
+
+    public Integer getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Integer priority) {
+        this.priority = priority;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getLastModifyTime() {
+        return lastModifyTime;
+    }
+
+    public void setLastModifyTime(Date lastModifyTime) {
+        this.lastModifyTime = lastModifyTime;
+    }
+}
